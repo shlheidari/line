30c27961de59d5dd2aa2c38348cb55561dfe3eb8
shlheidari <soheilheidari289@yahoo.com>
2022-09-18 22:48:23 +0430

    make member sql

--- myworld/line/models.py
+++ myworld/line/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+  name = models.CharField(max_length=255)
+  student_id = models.CharField(max_length=255)
+  password = models.CharField(max_length=255)
+  start_time = models.CharField(max_length=255)
+  end_time = models.CharField(max_length=255)
+  line = models.CharField(max_length=255)
+  health_dermatology_ent_one = models.CharField(max_length=255)
+  health_dermatology_ent_two = models.CharField(max_length=255)
+  health_dermatology_ent_three = models.CharField(max_length=255)
+  anesthesia_emergency_ophthalmology_one = models.CharField(max_length=255)
+  anesthesia_emergency_ophthalmology_two = models.CharField(max_length=255)
+  anesthesia_emergency_ophthalmology_three = models.CharField(max_length=255)
+  neurology_psychiatry_infectious_one = models.CharField(max_length=255)
+  neurology_psychiatry_infectious_two = models.CharField(max_length=255)
+  neurology_psychiatry_infectious_three = models.CharField(max_length=255)
+  ethics_radiology_one = models.CharField(max_length=255)
+  ethics_radiology_two = models.CharField(max_length=255)
+  orthopedics_urology_one = models.CharField(max_length=255)
+  orthopedics_urology_two = models.CharField(max_length=255)
+  internal = models.CharField(max_length=255)
+  surgery = models.CharField(max_length=255)
+  gynecology = models.CharField(max_length=255)
